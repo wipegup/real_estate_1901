1439fd1cb5160d9b611f0825488bc04541ee41e8
wipegup <wipegup@gmail.com>
2019-01-30 09:49:30 -0700

    Add Rooms by category, price per sqft and sorted by area tests

--- test/house_test.rb
+++ test/house_test.rb
@@ -71,4 +71,64 @@ class HouseTest < MiniTest::Test
 
     assert_equal true_area, @house.area
   end
+
+  def test_it_has_price_per_sq_foot
+    room_1 = Room.new(:bedroom, 10,13)
+    room_2 = Room.new(:bedroom, 11,15)
+    room_3 = Room.new(:bathroom, 6,10)
+    room_4 = Room.new(:living_room, 25,15)
+    room_5 = Room.new(:basement, 30,41)
+    room_list = [room_1,room_2,room_3,room_4,room_5]
+
+    room_list.each { |room| @house.add_room(room)}
+
+    true_area = room_list.map{|room| room.area}.sum
+
+    true_price_per_sq_ft = true_area.to_f / 400000
+
+    assert_equal true_price_per_sq_ft, @house.price_per_square_foot
+  end
+
+  def test_it_has_room_sorted_by_area
+    room_1 = Room.new(:bedroom, 10,13)
+    room_2 = Room.new(:bedroom, 11,15)
+    room_3 = Room.new(:bathroom, 6,10)
+    room_4 = Room.new(:living_room, 25,15)
+    room_5 = Room.new(:basement, 30,41)
+    room_list = [room_1,room_2,room_3,room_4,room_5]
+
+    room_list.each { |room| @house.add_room(room)}
+
+    sorted_rooms = room_list.sort_by{ |room| room.area}
+
+    @house.rooms_sorted_by_area.zip(sorted_rooms){
+      |house_room, sorted_room|
+      assert_equal sorted_room, house_room
+    }
+
+  end
+
+  def test_it_has_rooms_by_category
+    room_1 = Room.new(:bedroom, 10,13)
+    room_2 = Room.new(:bedroom, 11,15)
+    room_3 = Room.new(:bathroom, 6,10)
+    room_4 = Room.new(:living_room, 25,15)
+    room_5 = Room.new(:basement, 30,41)
+    room_list = [room_1,room_2,room_3,room_4,room_5]
+
+    room_list.each { |room| @house.add_room(room)}
+
+    room_by_cat = {}
+    room_list.each { |room|
+      if !room_by_cat.keys.include?(room.category)
+        room_by_cat[room.category] = [room]
+      else
+        room_by_cat << room
+      end
+    }
+
+    room_by_cat.keys.each{ |category|
+      assert_equal room_by_cat[category], @house.rooms_by_category[category]
+    }
+  end
 end
